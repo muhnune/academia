95450aab36cc103f5e179cb042473af9ab4e6a21
muhnunee <f1d022075@student.unram.ac.id>
2023-10-21 20:48:47 +0800

    first commit

---
+++ tipelaptop.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+// Kelas Parents (Superclass)
+class ProdukElektronik {
+    String merek;
+    int tahunProduksi;
+
+    public ProdukElektronik(String merek, int tahunProduksi) {
+        this.merek = merek;
+        this.tahunProduksi = tahunProduksi;
+    }
+
+    public void info() {
+        System.out.println("Merek: " + merek);
+        System.out.println("Tahun Produksi: " + tahunProduksi);
+    }
+}
+
+// Kelas Child (Subclass)
+class Laptop extends ProdukElektronik {
+    String tipe;
+
+    public Laptop(String merek, int tahunProduksi, String tipe) {
+        super(merek, tahunProduksi);
+        this.tipe = tipe;
+    }
+
+    public void info() {
+        super.info(); // Memanggil metode info() dari kelas induk
+        System.out.println("Tipe: " + tipe);
+    }
+}
+
+// Kelas Main (Kelas Utama)
+public class tipelaptop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Meminta input dari pengguna
+        System.out.print("Masukkan Merek Produk Elektronik: ");
+        String merek = input.nextLine();
+
+        System.out.print("Masukkan Tahun Produksi Produk Elektronik: ");
+        int tahunProduksi = input.nextInt();
+
+        input.nextLine(); // Membersihkan newline
+
+        System.out.print("Masukkan Tipe Laptop: ");
+        String tipe = input.nextLine();
+
+        // Membuat objek Laptop berdasarkan input pengguna
+        Laptop laptop = new Laptop(merek, tahunProduksi, tipe);
+
+        // Memanggil metode info() dari kelas Laptop
+        laptop.info();
+
+        input.close();
+    }
+}
